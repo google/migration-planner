cebb97d63f30b09f46a0d3f864b3abd63a37be31
Srishti Negi <srishtinegi@google.com>
2026-06-03 15:10:29 +0530

    perf: optimize retention policy powershell and power automate cloud flows detail fetching via parallel execution and bulk retrieval

--- core/powershell/scripts/get_retention_policies.ps1
+++ core/powershell/scripts/get_retention_policies.ps1
@@ -21,9 +21,9 @@ Import-Module ExchangeOnlineManagement
 # Connect to Security & Compliance PowerShell using App-Only Cert Auth
 if ($CertificatePassword) {
     $secPassword = ConvertTo-SecureString -String $CertificatePassword -AsPlainText -Force
-    Connect-IPPSSession -CertificateFilePath $CertificatePath -CertificatePassword $secPassword -AppId $AppId -Organization $Organization
+    Connect-IPPSSession -CertificateFilePath $CertificatePath -CertificatePassword $secPassword -AppId $AppId -Organization $Organization -ShowBanner:$false -WarningAction SilentlyContinue
 } else {
-    Connect-IPPSSession -CertificateFilePath $CertificatePath -AppId $AppId -Organization $Organization
+    Connect-IPPSSession -CertificateFilePath $CertificatePath -AppId $AppId -Organization $Organization -ShowBanner:$false -WarningAction SilentlyContinue
 }
 
 try {
@@ -32,24 +32,37 @@ try {
     $output = @()
 
     if ($policies) {
+        # Retrieve all compliance rules at once
+        $allRules = Get-RetentionComplianceRule
+        $ruleMap = @{}
+        if ($allRules) {
+            foreach ($rule in @($allRules)) {
+                if ($rule.Policy) {
+                    $policyKey = $rule.Policy.ToString()
+                    $ruleMap[$policyKey] = $rule
+                }
+            }
+        }
+
         # Handle case where $policies is not an array
         $policies_list = @($policies)
         
         foreach ($policy in $policies_list) {
-            # Get the rules associated with this policy
-            $rules = Get-RetentionComplianceRule -Policy $policy.Name
-            
             $duration = "N/A"
             $action = "N/A"
             $trigger = "N/A"
-            if ($rules) {
-                $rules_list = @($rules)
-                if ($rules_list.Count -gt 0) {
-                    $rule = $rules_list[0]
-                    $duration = $rule.RetentionDuration
-                    $action = $rule.RetentionAction
-                    $trigger = $rule.RetentionTrigger
-                }
+            
+            $rule = $null
+            if ($ruleMap.ContainsKey($policy.Name)) {
+                $rule = $ruleMap[$policy.Name]
+            } elseif ($ruleMap.ContainsKey($policy.Identity.ToString())) {
+                $rule = $ruleMap[$policy.Identity.ToString()]
+            }
+
+            if ($rule) {
+                $duration = $rule.RetentionDuration
+                $action = $rule.RetentionAction
+                $trigger = $rule.RetentionTrigger
             }
             
             # Construct a combined custom object
