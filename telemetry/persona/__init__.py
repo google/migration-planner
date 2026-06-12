c3fb35145a7a24d1087a3dcf2599915b2dcdda49
NityamPareek <nityampareek@google.com>
2026-06-12 11:17:01 +0530

    refactor: Modularize user persona analysis utilities and strategies under telemetry/persona/ submodule

---
+++ telemetry/persona/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .dataset_generator import generate_user_activity_dataset
+from .clustering import perform_kmeans_clustering, classify_users_to_personas
+from .gemini_clients import generate_personas_from_dataset, generate_kmeans_personas_gemini
